870b1de1a4a9460bf7c6adf4a6bd654bc41143aa
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-02-06 12:19:13 -0500

    Améliorer la performance et la réutilisabilité du SizePreservingSubViewportContainer (Et lui donner un nom)
    En plus, on utilise maintenant SubViewportContainer au lieu de
    TextureRect, donc les événements de souris sont propagé au subviewport

--- switch-ups-godot/globals/game_info.gd
+++ switch-ups-godot/globals/game_info.gd
@@ -3,5 +3,5 @@ class_name game_info
 
 static var project_name = "Switch-ups"
 static var version = "0"
-static var flavor = "Technical prototype (subviewport resizing)"
+static var flavor = "Technical prototype (subviewport resizing v2)"
 static var dev = true
--- switch-ups-godot/globals/simple_scripts/viewport_min_size.gd
+++ switch-ups-godot/globals/simple_scripts/viewport_min_size.gd
@@ -4,15 +4,12 @@ extends SubViewport
 var target_size = Vector2i(1920,1080)
 #var target_size = Vector2i(3840,2160) # 4k
 var target_aspect_ratio : float
-var last_window_size : Vector2i
-const MAX_VALUE = 16390
 
 func get_parent_viewport() -> Viewport :
 	return $"..".get_viewport()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	last_window_size = Vector2i(target_size)
 	target_aspect_ratio = float(target_size.x) / float(target_size.y)
 	resize()
 	pass # Replace with function body.
@@ -43,11 +40,6 @@ func resize() :
 	else :
 		width = floor(float(target_size.y) * window_aspect_ratio)
 		height = floor(float(target_size.y))
-	
-	if height >= MAX_VALUE or width >= MAX_VALUE :
-		DisplayServer.window_set_size(last_window_size)
-		print("Exceeded max aspect ratio!!! %v" % Vector2i(width,height))
-	else :
-		size = Vector2i(width,height)
-		last_window_size = Vector2i(window_size)
-		print("Window resized or scene entered tree! %s's size is now %v" % [name, size])
+
+	size_2d_override = Vector2i(width,height)
+	print("Window resized or scene entered tree! %s's size is now %v" % [name, size_2d_override])
