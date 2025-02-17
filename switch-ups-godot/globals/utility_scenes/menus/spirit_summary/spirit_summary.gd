1bbba90ec350725f4b03ed83727fc55dc4c2be1b
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-02-17 17:37:06 -0500

    Ajouter liste équipe active

--- switch-ups-godot/globals/utility_scenes/menus/spirit_summary/spirit_summary.gd
+++ switch-ups-godot/globals/utility_scenes/menus/spirit_summary/spirit_summary.gd
@@ -12,6 +12,7 @@ extends Control
 @export var move_holder : Container
 
 var spirit : ms_spirit
+var spirit_active : ms_spirit_active
 
 func _ready() -> void:
 	#set_summary_inactive(load("res://monster_systems/spirits/database/placeholder.tres"))
@@ -20,11 +21,31 @@ func _ready() -> void:
 func _notification(what: int) -> void:
 	match what :
 		NOTIFICATION_TRANSLATION_CHANGED:
-			if not spirit == null : set_summary_inactive(spirit)
-			# TODO Add spirit in active form
+			reload_content
+
+func reload_content() :
+	if spirit_active == null and not spirit == null : set_summary_inactive(spirit)
+	elif not spirit_active == null : set_summary_active(spirit_active)
+	else : set_empty()
+
+func set_empty() :
+	spirit = null
+	spirit_active = null
+	label_name.text = ""
+	sprite.texture = null
+	label_type.text = ""
+	label_health.text = ""
+	label_concrete.text = ""
+	label_abstract.text = ""
+	label_defense.text = ""
+	label_speed.text = ""
+	label_luck.text = ""
+	move_holder.set_actions([])
+	%ActionsLabel.hide()
 
 func set_summary_inactive(_spirit : ms_spirit) :
 	spirit = _spirit
+	spirit_active = null
 	label_name.text = "[center]{name}[/center]".format({"name":tr(spirit.name)})
 	sprite.texture = spirit.sprite
 	label_type.text = tr("TR_SUMMARY_TYPE").format({"type":ms_constants.type_to_bbcode(spirit.type)})
@@ -37,6 +58,18 @@ func set_summary_inactive(_spirit : ms_spirit) :
 	move_holder.set_actions(spirit.actions)
 	%ActionsLabel.show()
 
-func set_summary_active() :
-	printerr("set_summary_active() : Not implemented")
+func set_summary_active(_spirit : ms_spirit_active) :
+	spirit_active = _spirit
+	spirit = SpiritDictionary.spirits[spirit_active.key]
+	label_name.text = "[center]{name}[/center]".format({"name":tr(spirit.name)})
+	sprite.texture = spirit.sprite
+	label_type.text = tr("TR_SUMMARY_TYPE").format({"type":ms_constants.type_to_bbcode(spirit.type)})
+	label_health.text = tr("HP : {curr}/{max}").format({"curr":spirit_active.current_hp,"max":spirit.health})
+	label_concrete.text = tr("TR_SUMMARY_CONCRETE").format({"tr":ms_constants.flavor_to_bbcode(ms_constants.ATK_FLAVOR.CONCRETE),"power":spirit.atk_concrete})
+	label_abstract.text = tr("TR_SUMMARY_ABSTRACT").format({"tr":ms_constants.flavor_to_bbcode(ms_constants.ATK_FLAVOR.ABSTRACT),"power":spirit.atk_abstract})
+	label_defense.text = tr("TR_SUMMARY_DEFENSE").format({"defense":spirit.defense})
+	label_speed.text = tr("TR_SUMMARY_SPEED").format({"speed":spirit.speed})
+	label_luck.text = tr("TR_SUMMARY_LUCK").format({"luck":spirit.luck})
+	move_holder.set_actions(spirit_active.get_moves())
+	%ActionsLabel.show()
 	pass
--- switch-ups-godot/monster_systems/spirit_active.gd
+++ switch-ups-godot/monster_systems/spirit_active.gd
@@ -3,17 +3,20 @@ extends RefCounted
 class_name ms_spirit_active
 
 var key : String
+var key_equip : String
 var current_hp : int
-var moves : Array[String]
+var moves : Array
 
 func read_dict(_data : Dictionary) -> void :
 	key = _data["key"]
+	key_equip = _data["key_equip"]
 	current_hp = _data["hp"]
 	moves = _data["moves"]
 
 func to_dict() -> Dictionary :
 	return {
 		"key" = key,
+		"key_equip" = key_equip,
 		"hp" = current_hp,
 		"moves" = moves,
 	}
@@ -24,3 +27,14 @@ static func from_dict(_data : Dictionary) -> ms_spirit_active :
 	var instance = ms_spirit_active.new()
 	instance.read_dict(_data)
 	return instance
+
+static func from_spirit(_spirit : ms_spirit) -> ms_spirit_active :
+	var instance = ms_spirit_active.new()
+	instance.current_hp = _spirit.health
+	instance.key = _spirit.key
+	instance.moves = []
+	instance.key_equip = ""
+	return instance
+
+func get_moves() -> Array[ms_action] :
+	return []
