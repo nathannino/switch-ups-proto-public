ea5dc37d34c573d1d92caf040990d39eb7df59bd
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-03-23 23:13:00 -0400

    Désactiver le bouton de confirmation de sp_switch_data à l'initialization

--- switch-ups-godot/globals/game_info.gd
+++ switch-ups-godot/globals/game_info.gd
@@ -2,6 +2,6 @@ extends Object
 class_name game_info
 
 const project_name = "Switch-ups"
-const version = "2" # Used for client server compatibility
+const version = "3" # Used for client server compatibility
 const flavor = "Technical prototype (uiv2)"
 const dev = true
