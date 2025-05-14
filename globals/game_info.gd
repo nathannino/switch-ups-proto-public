443e17f8f59ee452a9b6499b6dd9a062624dc675
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-05-14 14:38:31 -0400

    bugfix certaines actions avec des composantes erroné

--- globals/game_info.gd
+++ globals/game_info.gd
@@ -2,6 +2,6 @@ extends Object
 class_name game_info
 
 const project_name = "Switch-ups"
-const version = "SchoolV5-Vanilla" # Used for client server compatibility
-const flavor = "School Release"
+const version = "SchoolV5.1-Vanilla" # Used for client server compatibility
+const flavor = "School Release (bugfix)"
 const dev = false
